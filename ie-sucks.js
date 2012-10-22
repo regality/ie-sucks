c977857ff1d36679659d20d4e00b52ef122a78b3
Adam Blackburn <regality@gmail.com>
2012-10-21 21:55:44 -0600

    renaming bundles

--- ie-sucks.js
+++ ie-sucks.js
@@ -1,1 +1,219 @@
-Array.prototype.every||(Array.prototype.every=function(e,n){if(typeof e!="function")throw new TypeError(e+" is not a function");typeof n=="undefined"&&(n=this);for(var r=0,i=this.length;r<i;r++)if(r in t&&!e.call(n,this[r],r,this))return!1;return!0}),Array.prototype.filter||(Array.prototype.filter=function(e,t){if(typeof e!="function")throw new TypeError(e+" is not a function");typeof t=="undefined"&&(t=this);var n=[];for(var r=0,i=this.length;r<i;++r)this.hasOwnProperty(r)&&e.call(t,this[r],r,this)&&n.push(val);return n}),Array.prototype.forEach||(Array.prototype.forEach=function(e,t){if(typeof e!="function")throw new TypeError(e+" is not a function");typeof t=="undefined"&&(t=this);for(var n=0,r=this.length;n<r;++n)e.call(t,this[n],n,this)}),Array.prototype.indexOf||(Array.prototype.indexOf=function(e,t){if(!t||!isFinite(t))t=0;for(var n=t,r=this.length;n<r;++n)if(this.hasOwnProperty(n)&&this[n]===e)return n;return-1}),Array.prototype.lastIndexOf||(Array.prototype.lastIndexOf=function(e,t){if(!t||!isFinite(t))t=this.length-1;for(var n=t;n>=0;--n)if(this.hasOwnProperty(n)&&this[n]===e)return n;return-1}),Array.prototype.map||(Array.prototype.map=function(e,t){if(typeof e!="function")throw new TypeError(e+" is not a function");typeof t=="undefined"&&(t=this);var n=new Array(this.length);for(var r=0,i=this.length;r<i;++r)n[r]=e.call(t,this[r],r,this);return n}),Array.prototype.reduce||(Array.prototype.reduce=function(t,n){if(typeof t!="function")throw new TypeError(t+" is not a function");for(var r=0,i=this.length;r<i;++r)this.hasOwnProperty(r)&&(n=t.call(this,n,this[r],r,this));return n}),Array.prototype.reduceRight||(Array.prototype.reduceRight=function(t,n){if(typeof t!="function")throw new TypeError(t+" is not a function");for(var r=this.length-1;r>=0;++r)this.hasOwnProperty(r)&&(n=t.call(this,n,this[r],r,this));return n}),Array.prototype.some||(Array.prototype.some=function(e,t){if(typeof e!="function")throw new TypeError(e+" is not a function");typeof t=="undefined"&&(t=this);for(var n=0,r=this.length;n<r;n++)if(this.hasOwnProperty(n)&&e.call(t,this[n],n,this))return!0;return!1}),Function.prototype.bind||(Function.prototype.bind=function(){if(typeof this!="function")throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");var e=this,t=arguments.length===1?[arguments[0]]:Array.apply(null,arguments),n=t.shift();if(t.length)var r=function(){return e.apply(n,t)};else var r=function(){return e.apply(n,arguments)};return r.prototype=this.prototype,r}),Object.prototype.create||(Object.create=function(e){function t(){}if(arguments.length>1)throw new Error("Object.create implementation only accepts the first parameter.");return t.prototype=e,new t}),Object.prototype.keys||(Object.keys=function(e){if(typeof e!="object"&&typeof e!="function"||e===null)throw new TypeError("Object.keys called on non-object");var t=[];for(var n in e)e.hasOwnProperty(n)&&t.push(n);return t});
+if (!Array.prototype.every) {
+Array.prototype.every = function(fn, context) {
+  if (typeof fn != "function") {
+    throw new TypeError(fn + " is not a function");
+  }
+
+  if (typeof context === 'undefined') {
+    context = this;
+  }
+
+  for (var i = 0, l = this.length; i < l; i++) {
+    if (i in t && !fn.call(context, this[i], i, this)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+}
+
+if (!Array.prototype.filter) {
+Array.prototype.filter = function(fn, context) {
+  if (typeof fn != "function") {
+    throw new TypeError(fn + " is not a function");
+  }
+
+  if (typeof context === 'undefined') {
+    context = this;
+  }
+
+  var res = [];
+  for (var i = 0, l = this.length; i < l; ++i) {
+    if (this.hasOwnProperty(i)) {
+      if (fn.call(context, this[i], i, this)) {
+        res.push(val);
+      }
+    }
+  }
+
+  return res;
+}
+}
+
+if (!Array.prototype.forEach) {
+Array.prototype.forEach = function(fn, context) {
+  if (typeof fn != "function") {
+    throw new TypeError(fn + " is not a function");
+  }
+
+  if (typeof context === 'undefined') {
+    context = this;
+  }
+
+  for (var i = 0, l = this.length; i < l; ++i) {
+    fn.call(context, this[i], i, this);
+  }
+}
+}
+
+if (!Array.prototype.indexOf) {
+Array.prototype.indexOf = function(searchElement, fromIndex) {
+  if (!fromIndex || !isFinite(fromIndex)) {
+    fromIndex = 0;
+  }
+
+  for (var i = fromIndex, l = this.length; i < l; ++i) {
+    if (this.hasOwnProperty(i) && this[i] === searchElement) {
+      return i;
+    }
+  }
+  return -1;
+}
+}
+
+if (!Array.prototype.lastIndexOf) {
+Array.prototype.lastIndexOf = function(searchElement, fromIndex) {
+  if (!fromIndex || !isFinite(fromIndex)) {
+    fromIndex = this.length - 1;
+  }
+
+  for (var i = fromIndex; i >= 0; --i) {
+    if (this.hasOwnProperty(i) && this[i] === searchElement) {
+      return i;
+    }
+  }
+  return -1;
+}
+}
+
+if (!Array.prototype.map) {
+Array.prototype.map = function(fn, context) {
+  if (typeof fn != "function") {
+    throw new TypeError(fn + " is not a function");
+  }
+
+  if (typeof context === 'undefined') {
+    context = this;
+  }
+
+  var result = new Array(this.length);
+  for (var i = 0, l = this.length; i < l; ++i) {
+    result[i] = fn.call(context, this[i], i, this);
+  }
+
+  return result;
+}
+}
+
+if (!Array.prototype.reduce) {
+Array.prototype.reduce = function reduce(fn, previous) {
+
+  if (typeof fn != "function") {
+    throw new TypeError(fn + " is not a function");
+  }
+
+  for (var i = 0, l = this.length; i < l; ++i) {
+    if (this.hasOwnProperty(i)) {
+      previous = fn.call(this, previous, this[i], i, this);
+    }
+  }
+
+  return previous;
+}
+}
+
+if (!Array.prototype.reduceRight) {
+Array.prototype.reduceRight = function reduce(fn, previous) {
+
+  if (typeof fn != "function") {
+    throw new TypeError(fn + " is not a function");
+  }
+
+  for (var i = this.length - 1; i >= 0; ++i) {
+    if (this.hasOwnProperty(i)) {
+      previous = fn.call(this, previous, this[i], i, this);
+    }
+  }
+
+  return previous;
+}
+}
+
+if (!Array.prototype.some) {
+Array.prototype.some = function(fn , context) {
+  if (typeof fn != "function") {
+    throw new TypeError(fn + " is not a function");
+  }
+
+  if (typeof context === 'undefined') {
+    context = this;
+  }
+
+  for (var i = 0, l = this.length; i < l; i++) {
+    if (this.hasOwnProperty(i) && fn.call(context, this[i], i, this)) {
+      return true;
+    }
+  }
+
+  return false;
+}
+}
+
+if (!Function.prototype.bind) {
+Function.prototype.bind = function () {
+  if (typeof this !== "function") {
+    throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+  }
+
+  var self = this;
+  var args = arguments.length === 1
+           ? [arguments[0]]
+           : Array.apply(null, arguments);
+  var context = args.shift();
+
+  if (args.length) {
+    var bound = function() {
+      return self.apply(context, args);
+    }
+  } else {
+    var bound = function() {
+      return self.apply(context, arguments);
+    }
+  }
+
+  bound.prototype = this.prototype;
+
+  return bound;
+}
+}
+
+if (!Object.prototype.create) {
+Object.create = function (o) {
+  if (arguments.length > 1) {
+    throw new Error('Object.create implementation only accepts the first parameter.');
+  }
+  function F() {}
+  F.prototype = o;
+  return new F();
+}
+}
+
+if (!Object.prototype.keys) {
+Object.keys = function (obj) {
+  if (typeof obj !== 'object' && typeof obj !== 'function' || obj === null) {
+    throw new TypeError('Object.keys called on non-object');
+  }
+
+  var result = []
+
+  for (var prop in obj) {
+    if (obj.hasOwnProperty(prop)) {
+      result.push(prop);
+    }
+  }
+
+  return result
+}
+}
+
